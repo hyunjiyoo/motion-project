a607555288fa2e7d37eba39401579c3c051519e6
hyunjiyoo <solteee23@gmail.com>
2021-10-26 15:03:53 +0900

    footer memo function 적용 &  video URL convert 함수 추가

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { memo } from 'react';
 import styles from './footer.module.css';
 
-const Footer = () => (
+const Footer = memo(() => (
   <footer className={styles.container}>
     <span className={styles.span}>ⓒ Copyright by Hyunji Yoo. All rights reversed.</span>
   </footer>
-);
+));
 
 export default Footer;
--- src/components/item/item.jsx
+++ src/components/item/item.jsx
@@ -4,23 +4,34 @@ import styles from './item.module.css';
 const Item = memo(({ item, onDeleteItem }) => {
   const { id, title, contents } = item;
   
-  const ImageItem = () => {
-    return (
-      <div className={styles.mediaContainer}>
-        <img className={styles.img} src={contents} alt={title} />
-        <p className={styles.title} >{title}</p>
-      </div>
-    );
+  const convertVideoURL = (url) => {
+    const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9(-|_)]{11}))|(?:youtu.be\/([a-zA-Z0-9(-|_)]{11})))/;
+    const match = url.match(regExp);
+
+    const videoId = match ? match[1] || match[2] : undefined;
+    if (videoId) {
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
+    return url;
   }
 
+  const ImageItem = () => (
+    <div className={styles.mediaContainer}>
+      <img className={styles.img} src={contents} alt={title} />
+      <p className={styles.title} >{title}</p>
+    </div>
+  );
+
   const VideoItem = () => {
+    const url = convertVideoURL(contents);
+    
     return (
       <div className={styles.mediaContainer}>
         <iframe
           className={styles.video}
           title="youtube video"
           type="text/html"
-          src={contents}
+          src={url}
           frameBorder="0"
         >
         </iframe>
@@ -29,24 +40,20 @@ const Item = memo(({ item, onDeleteItem }) => {
     );
   }
 
-  const NoteItem = () => {
-    return (
-      <div className={styles.textContainer}>
-        <p className={styles.title}>{title}</p>
-        <p className={styles.contents}>{contents}</p>
-      </div>
-    );
-  }
+  const NoteItem = () => (
+    <div className={styles.textContainer}>
+      <p className={styles.title}>{title}</p>
+      <p className={styles.contents}>{contents}</p>
+    </div>
+  );
 
-  const TaskItem = () => {
-    return (
-      <div className={styles.textContainer}>
-        <p className={styles.title}>{title}</p>
-        <input className={styles.checkbox} type="checkbox" id={id} />
-        <label htmlFor={id} className={styles.contents}>{contents}</label>
-      </div>
-    );
-  }
+  const TaskItem = () => (
+    <div className={styles.textContainer}>
+      <p className={styles.title}>{title}</p>
+      <input className={styles.checkbox} type="checkbox" id={id} />
+      <label htmlFor={id} className={styles.contents}>{contents}</label>
+    </div>
+  );
 
   const SelectedItem = () => {
     switch (item.type) {
